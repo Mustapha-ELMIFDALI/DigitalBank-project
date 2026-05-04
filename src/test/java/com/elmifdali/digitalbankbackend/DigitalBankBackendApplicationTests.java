9c61741fa4ed19577c39379c71beb88e24a85a75
Mustapha-ELMIFDALI <mustaphamifdali2001@gmail.com>
2026-05-04 18:00:00 +0100

    test: Ajout des tests unitaires

---
+++ src/test/java/com/elmifdali/digitalbankbackend/DigitalBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmifdali.digitalbankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
