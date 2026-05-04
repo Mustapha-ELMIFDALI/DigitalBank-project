6f43f7213ec02cd9534e650ec80a203f4a929d82
Mustapha-ELMIFDALI <mustaphamifdali2001@gmail.com>
2026-05-04 09:45:00 +0100

    feat: Ajout des entités JPA (Customer, BankAccount, AccountOperation)

---
+++ src/main/java/com/elmifdali/digitalbankbackend/enums/AccountStatus.java
@@ -0,0 +1,7 @@
+package com.elmifdali.digitalbankbackend.enums;
+
+public enum AccountStatus {
+    CREATED,
+    ACTIVATED,
+    SUSPENDED
+}
---
+++ src/main/java/com/elmifdali/digitalbankbackend/enums/OperationType.java
@@ -0,0 +1,6 @@
+package com.elmifdali.digitalbankbackend.enums;
+
+public enum OperationType {
+    CREDIT ,
+    DEBIT
+}
